e2394e43e9e2cc85d1806f97ce91af4a5df0f3e8
Faitsumaru <kiryakov.g.m@gmail.com>
2024-02-12 11:19:31 +0300

    smooth scroll (gsap plugin) & transitions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 window.addEventListener('scroll', e => {
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+
+//smooth scroll w/ gsap plugin:
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
 })
